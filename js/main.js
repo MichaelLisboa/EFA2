c7fe6951182550385cef29af0bbd302ebaf7a65f
Michael Lisboa <michael.lisboa@gmail.com>
2018-04-13 11:08:46 +0700

    added swipe JS for mobile

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $('.modal').on('shown.bs.modal', function() {
               'min-width': '10px'
    });
 });
+
+
+$(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
