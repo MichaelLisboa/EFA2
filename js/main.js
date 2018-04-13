69786d82e53dfe77357813fa57b830506e5a63ce
Michael Lisboa <michael.lisboa@gmail.com>
2018-04-13 12:50:52 +0700

    adding active state to mobile menu

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
-$('.navbar-nav a').on('click', function() {
-    console.log("CLICK ON NAV");
+$('ul.navbar-nav li a').click(function(e) {
+	var $this = $(this);
+	$this.parent().siblings().removeClass('active').end().addClass('active');
+
 	if ($('#topNavbarResponsive').css('display') != 'hide') {
 		$('#topNavbarResponsive').trigger("click");
-        var className = $('.navbar-collapse').attr('class');
-        console.log("CLASS", className);
-        $('#topNavbarResponsive').removeClass("show")
-        $('#topNavbarResponsive').addClass("hide")
+
+		setTimeout(function() {
+			$('#topNavbarResponsive').removeClass("show");
+			$('#topNavbarResponsive').addClass("hide");
+            // $('#topNavbarResponsive').fadeOut( "slow");
+		}, 330);
 	}
+	e.preventDefault();
 });
 
 $('.modal').on('shown.bs.modal', function() {
-   $(".modal-body").css("margin",'0px');
-   $(".modal-dialog").css({
-              'position': 'relative',
-              'display': 'table',
-              'overflow-y': 'auto',
-              'overflow-x': 'auto',
-              'width': 'auto',
-              'min-width': '10px'
-   });
+	$(".modal-body").css("margin", '0px');
+	$(".modal-dialog").css({
+		'position': 'relative',
+		'display': 'table',
+		'overflow-y': 'auto',
+		'overflow-x': 'auto',
+		'width': 'auto',
+		'min-width': '10px'
+	});
 });
 
 
-$(".carousel").on("touchstart", function(event){
-        var xClick = event.originalEvent.touches[0].pageX;
-    $(this).one("touchmove", function(event){
-        var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
-            $(this).carousel('next');
-        }
-        else if( Math.floor(xClick - xMove) < -5 ){
-            $(this).carousel('prev');
-        }
-    });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
-    });
+$(".carousel").on("touchstart", function(event) {
+	var xClick = event.originalEvent.touches[0].pageX;
+	$(this).one("touchmove", function(event) {
+		var xMove = event.originalEvent.touches[0].pageX;
+		if (Math.floor(xClick - xMove) > 5) {
+			$(this).carousel('next');
+		} else if (Math.floor(xClick - xMove) < -5) {
+			$(this).carousel('prev');
+		}
+	});
+	$(".carousel").on("touchend", function() {
+		$(this).off("touchmove");
+	});
 });
